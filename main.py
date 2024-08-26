72270811cbd39c32e240c40e3b5330d3a0eecd89
olenapolianska <addresing88@gmail.com>
2024-08-26 16:32:34 +0300

    smartnote

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from PyQt6.QtWidgets import *
 
+from file_help import *
 
+notes = read_from_file()
+print(notes)
 app = QApplication([])
 window = QWidget()
 
 
 text_edit = QTextEdit()
+
+
 list_notes_lbl = QLabel("список заміток")
 notes_list = QListWidget()
+notes_list.addItems(notes)
 create_note_btn = QPushButton("створити замітку")
 delete_note_btn = QPushButton("видалити замітку")
 save_note_btn = QPushButton("зберегти замітку")
@@ -42,5 +48,19 @@ main_line.addLayout(v1)
 
 
 window.setLayout(main_line)
+
+def show_note():
+    key = notes_list.selectedItems()[0].text()
+    text_edit.setText(notes[key]['текст'])
+    tegs_list.clear()
+    tegs_list.addItems(notes[key]['теги'])
+
+def save_note():
+    key = notes_list.selectedItems()[0].text()
+    notes[key] = text_edit.toPlainText()
+    write_in_file(notes)
+
+save_note_btn.clicked.connect(save_note)
+notes_list.itemClicked.connect(show_note)
 window.show()
 app.exec()
